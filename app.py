3cee383722a2c6363614ec8c30f7391fa6267221
TurtleMaster69420 <48506404+TurtleMaster69420@users.noreply.github.com>
2020-09-01 09:50:24 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, redirect, render_template
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -16,6 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(320), unique=True, nullable=False)
     name = db.Column(db.String(50), unique=False, nullable=False)
+    type = db.Column(db.String(50), unique=False, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.datetime.now)
     pwHash = db.Column(db.String(60), unique=False, nullable=False)
     orders = db.relationship("Order", backref="orderer", lazy=True)
@@ -45,33 +46,47 @@ class Item(db.Model):
     orders = db.relationship("Order", backref="item", lazy=True)
 
 
+def authenticate(page_type=None):
+    """
+    :param page_type: either orderer, staff, manager or a NoneType
+    :return: a tuple, the first value being a boolean indicating whether the user should be on the current page and the
+    second being a string (None if the boolean was set to true) indicating the path the user should be redirected to
+    """
+    pages = {None: "/login",
+             "orderer": "/orderer/home",
+             "staff": "/staff/home",
+             "manager": "/manager/home"}
+    if "user" not in session:
+        return not page_type, pages[None]
+    user = User.query.get(session["user"])
+    return user.type == page_type, user.pages[user.type]
+
+
 def create_user(name, email, password):
-    '''
+    """
     :param name: The user's real name
     :param email: The user's email address (used to contact them)
     :param password: The password the user set
     :return: A tuple (`result`, `message`), where `result` is a boolean indicating whether the user was created or not, and
     `message` is the message that will be displayed to the user
-    '''
+    """
     if User.query.filter_by(email=email).first():
         return False, "That email is already in use"
     pwHash = bcrypt.generate_password_hash(password, 10)
-    db.session.add(User(name=name, email=email, pwHash=pwHash))
+    db.session.add(User(name=name, email=email, pwHash=pwHash, type="user"))
     db.session.commit()
     return True, "Your account was created!"
 
 
-def verify_login(username, password):
-    '''
-    :param username: Either the name or email of the user. Both will checked in the database
+def verify_login(email, password):
+    """
+    :param email: The user's email
     :param password: The password which will be checked
     :return: `result`, which is a boolean indicating whether the login credentials were correct or not
-    '''
-    user = User.query.filter_by(name=username).first()
+    """
+    user = User.query.filter_by(email=email).first()
     if not user:
-        user = User.query.filter_by(email=username).first()
-        if not user:
-            return False
+        return False
 
     if bcrypt.check_password_hash(user.pwHash, password):
         return True
@@ -80,8 +95,51 @@ def verify_login(username, password):
 
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index():  # NOTE: session is cleared when the BROWSER is closed, not the last window of the page
+    allowed, new_page = authenticate()
+    if allowed:
+        return redirect("/login")
+    else:
+        return redirect(new_page)
+
+
+@app.route('/login')
+def login():
+    allowed, new_page = authenticate()
+    if not allowed:
+        return redirect(new_page)
+    return render_template("login.html")
+
+
+@app.route('/api/login', methods=["POST"])
+def api_login():  # TODO: replace login stub with functional code
+    return '{"success": "true"}'
+
+
+@app.route('/register')
+def register():
+    allowed, new_page = authenticate()
+    if not allowed:
+        return redirect(new_page)
+    return render_template("register.html")
+
+
+@app.route('/api/register')
+def api_register():  # TODO: replace register stub with functional code
+    return '{"success": "true"}'
+
+
+@app.route('/forgot_password')
+def forgot_password():
+    allowed, new_page = authenticate()
+    if not allowed:
+        return redirect(new_page)
+    return render_template("forot_password")
+
+
+@app.route('/api/forgot_password')
+def api_forgot_password():  # TODO: replace 'forgot password' stub with functional code
+    return '{"success": "true"}'
 
 
 if __name__ == '__main__':
